bd7136a865b9f22ea4b16a98207ec71f32238ba8
GutembergAmorim <gutemberg.py@gmail.com>
2025-08-10 14:52:39 -0300

    esqueleto do backend

---
+++ routes/private.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { Prisma } from "@prisma/client";
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany({ omit: { password: true } });
+
+    res.status(200).json({ messeger: "Usuarios encontratos", users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ messeger: "Falha no servidor", error: error.messeger });
+  }
+});
+
+export default router;
---
+++ routes/public.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "../generated/prisma/client.js";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+//Cadastro de usuário
+router.post("/register", async (req, res) => {
+  try {
+    const user = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(user.password, salt);
+
+    const newUser = await prisma.user.create({
+      data: {
+        name: user.name,
+        cpf: user.cpf,
+        email: user.email,
+        password: hashPassword,
+      },
+    });
+    // Removendo a senha da resposta para não expor o hash
+    const { password, ...userWithoutPassword } = newUser;
+    res.status(201).json(userWithoutPassword);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao cadastrar usuário", error: error.message });
+  }
+});
+
+//Login de usuário
+router.post("/login", async (req, res) => {
+  try {
+    const userInfo = req.body;
+
+    // Verifica se o usuário existe
+    const user = await prisma.user.findUnique({
+      where: { email: userInfo.email },
+    });
+
+    // Se o usuário não existir, retorna erro
+    if (!user) {
+      return res.status(404).json({ message: "Email ou senha inválidos" });
+    }
+
+    // Verifica se a senha está correta
+    const validPassword = await bcrypt.compare(
+      userInfo.password,
+      user.password
+    );
+
+    // Se a senha estiver incorreta, retorna erro
+    if (!validPassword) {
+      return res.status(401).json({ message: "Email ou senha inválidos" });
+    }
+
+    // Gerar token JWT e remover a senha da resposta
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1m" });
+    const { password, ...userWithoutPassword } = user;
+    res.status(200).json({ message: "Login bem-sucedido", token, user: userWithoutPassword });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Erro ao fazer login", error: error.message });
+  }
+});
+
+export default router;
